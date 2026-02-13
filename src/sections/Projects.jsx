daca2ab32a626a2189764d69e53074754834a12d
ItsArgon <nrfunk5@gmail.com>
2026-02-12 21:06:18 -0600

    Add more project github links and reorganize projects

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -5,11 +5,25 @@ import {
 export default function Projects() {
   const projects = [
     {
-      title: "Board Game Connections App",
+      title: "ArgonMod - Minecraft Mod",
+      description: 
+        "A Minecraft mod that performs an automatic fishing action based on viewing an armorstand marked with a specific tag. Also includes a custom command, config file, and GUI for adjusting settings. Currently made using Forge for Minecraft 1.8.9, but will be ported to 1.21.10 and Fabric.",
+      tech: "Java · Minecraft Modding · Forge",
+      link: "https://github.com/ItsArgon/ArgonMod.git"
+    },
+    {
+      title: "BoardUp - Board Game Meetup App",
       description:
         "A web and Android app for board game enthusiasts to find local gaming groups, events, and host their own games.",
       tech: "Flutter · Firebase · Android · Architecture",
-      link: ""
+      link: "https://github.com/ItsArgon/BoardUp.git"
+    },
+    {
+      title: "Guitar Tabs App (Songsterr Clone)",
+      description:
+        "Another upcoming project showcasing problem solving, performance, and system design skills.",
+      tech: "React · Firebase · Web Development",
+      link: "https://github.com/ItsArgon/EasyTabs.git"
     },
     {
       title: "Multi-Round Cipher",
@@ -17,20 +31,6 @@ export default function Projects() {
         "A multi-round cipher based on Vigenere and Columnar Transposition ciphers, designed to encrypt different blocks with a basic level of security. Not meant to be used for real security, but to demonstrate the design and implementation of a custom cipher system.",
       tech: "Java · Algorithms · Systems",
       link:"https://github.com/ItsArgon/Multi-round-Cipher.git"
-    },
-    {
-      title: "Guitar Tabs App (Songsterr Clone)",
-      description:
-        "Another upcoming project showcasing problem solving, performance, and system design skills.",
-      tech: "React · Firebase · Web Development",
-      link: ""
-    },
-    {
-      title: "ArgonMod - Minecraft Mod",
-      description: 
-        "A Minecraft mod that performs an automatic fishing action based on viewing an armorstand marked with a specific tag. Also includes a custom command, config file, and GUI for adjusting settings. Currently made using Forge for Minecraft 1.8.9, but will be ported to 1.21.10 and Fabric.",
-      tech: "Java · Minecraft Modding · Forge",
-      link: "https://github.com/ItsArgon/ArgonMod.git"
     },
   ]
 
